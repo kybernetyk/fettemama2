3646b21cea13bd66da618a6a5f2ea77cfad2eaf0
Leon Szpilewski <leon.szpilewski@gmail.com>
2012-02-17 20:01:59 +0100

    omg html is best embedded in c++

--- src/blog_index.cc
+++ src/blog_index.cc
@@ -5,13 +5,7 @@
 #include "blog_index.h"
 #include "database.h"
 
-lipido::WebResponse handleIndex(lipido::WebContext &ctx) {
-	lipido::WebResponse response;
-	MySQLDatabase db;
-
-	//TODO: move this html generation to a template system ... !!!
-	std::stringstream body;
-
+static void render_head(std::stringstream &body) {
 	body << "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//ENi\">";
 	body << "<html>";
 	body << "<head>";
@@ -24,7 +18,19 @@ lipido::WebResponse handleIndex(lipido::WebContext &ctx) {
 	body << "<p align=right>Fragen? <a href=/faq.html>Antworten!</a><br>";
 	body << "<i>Einsendungen und Beschwerden bitte an blog@fettemama.org</i></p>";
 	body << "<p align=right>IRC: irc.hackint.eu | #fm</p>";
+}
 
+static void render_ass(std::stringstream &body) {
+	body << "<div align=right>Proudly made without PHP, Java, Perl, and Postgres<br><a href=/impressum.html>Impressum</a></div>";
+	body << "</body></html>";
+}
+
+static lipido::WebResponse render_frontpage(lipido::WebContext &ctx) {
+	lipido::WebResponse response;
+	MySQLDatabase db;
+
+	std::stringstream body;
+	render_head(body);
 	body << "<ul>";
 
 	std::vector<std::map<std::string, std::string>> posts;
@@ -37,10 +43,8 @@ lipido::WebResponse handleIndex(lipido::WebContext &ctx) {
 	}
 
 	std::string day = "";
-
 	for (auto post : posts) {
 		std::string postdate = post["timestamp"];
-
 		if (day != postdate) {
 			day = postdate;
 			body << "</ul>";
@@ -48,27 +52,40 @@ lipido::WebResponse handleIndex(lipido::WebContext &ctx) {
 			body << postdate;
 			body << "</h2><ul>";
 		}
-
 		body << "<li><p>" << post["content"] << "</p></li>";
 	}
-
+	
 	body << "</ul>";
-
-	body << "<hr>Debug:<hr>";
-
-	if (ctx.request.params.size()) {
-		body << "GET params:<pre>\n";
-
-		for (auto param : ctx.request.params) {
-			body << param.first << " -> " << param.second << "\n";
-		}
-
-		body << "</pre>";
-	}
-
-	body << "</body></html>";
+	render_ass(body);
 	response.body = body.str();
 	return response;
 }
 
+static lipido::WebResponse render_niy(lipido::WebContext &ctx) {
+	lipido::WebResponse response;
+	std::stringstream body;
+	render_head(body);
 
+	body << "<pre>\n";
+	body << "Not implemented yet!\n";
+
+	body << "Debug:" << std::endl;
+	if (ctx.request.params.size()) {
+		body << "GET params:\n";
+		for (auto param : ctx.request.params) {
+			body << param.first << " -> " << param.second << std::endl;
+		}
+	}
+	body << "</pre>\n";
+	render_ass(body);
+	response.body = body.str();
+	return response;
+}
+
+lipido::WebResponse handleIndex(lipido::WebContext &ctx) {
+	if (ctx.request.params.size() > 0) {
+		return render_niy(ctx);
+	}
+
+	return render_frontpage(ctx);
+}
