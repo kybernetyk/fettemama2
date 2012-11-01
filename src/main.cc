80c44521325361fe87e5bd976de9e83fbee94166
Leon Szpilewski <leon.szpilewski@gmail.com>
2012-11-01 11:30:35 +0100

    fixed to work with clang

--- src/main.cc
+++ src/main.cc
@@ -45,20 +45,10 @@ int main(int argc, char **argv) {
 	server.addGetHandler("/index.php/feed/atom/", handleRSS);
 	server.addGetHandler("/feed/rss2/", handleRSS);
 	server.addGetHandler("/feed/", handleRSS);
-	/*
-	    server.addGetHandler("/fefe", [](lipido::WebContext &context) -> lipido::WebResponse {
-	        printf("fefe ist fett\n");
-	        lipido::WebResponse r;
-	        r.body = "<h1>Fefe ist ein Ferkel!</h1> LOL!";
-	        return r;
-	    });
-		server.addPostHandler("/post", post_handler);
-	*/
-	auto if1 = std::pair<std::string,unsigned short>("0.0.0.0", 80);
-	auto if2 = std::pair<std::string, unsigned short>("fc0a:ef4c:adff:e2f9:639f:7f79:cc16:99db", 80);
-	auto interfaces = std::vector<std::pair<std::string, unsigned short>>;
-	interfaces.push_bak(if1);
-	interfaces.push_bak(if2);
+
+	std::pair<std::string,unsigned short> if1 = std::make_pair("0.0.0.0", 9876);
+	auto interfaces = std::vector<std::pair<std::string, unsigned short>>();
+	interfaces.push_back(if1);
 	server.run(interfaces);
 }
 
