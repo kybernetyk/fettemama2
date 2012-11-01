666999d89d7521ea15520a70514b883c8c4166f9
Leon Szpilewski <leon.szpilewski@gmail.com>
2012-11-01 10:37:31 +0100

    analyticks:wq

--- src/blog_index.cc
+++ src/blog_index.cc
@@ -12,6 +12,8 @@ static void render_head(std::stringstream &body) {
 	body << "<link rel='alternate' type='application/rss+xml' title='RSS FEED DU AFFE' href='http://feeds.feedburner.com/FetteMama'>";
 	body << "<title>fefemama.org - THE BEST BLOG IN THE UNIVERSE WRITTEN IN C++ :]</title>";
 //	body << "<body>";
+	body << "<script type=\"text/javascript\">var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-705689-1']);_gaq.push(['_setDomainName', 'fettemama.org']);_gaq.push(['_trackPageview']);(function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();</script>";
+	body << "</head>";
 	body << "<body link=#ffffff vlink=#ffffff alink=#ffff00 bgcolor=#000000 text=#00dd00>";
 	body << "<font face='terminal, system, monospace'>";
 	body << "<table><td width=800>";
